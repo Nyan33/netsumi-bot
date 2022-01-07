4f2fa8949fc2b99b460b465cb512739e41508eaa
Nyan33 <volfnikita1304@gmail.com>
2022-01-07 17:30:01 +0300

    fixes

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.names = exports.run = void 0;
 function run(e) {
-    e.message.reply(`Команды:\nахегао- отправляет Ахего лицо из символов\nкарта- вытянуть случайную карту из колоды`);
+    e.message.reply(`Команды:\nахегао- отправляет ахегао лицо из символов\nкарта- вытянуть случайную карту из колоды`);
 }
 exports.run = run;
 exports.names = ["хелп"];
