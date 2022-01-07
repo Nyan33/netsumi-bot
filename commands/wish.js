e7078212a0ce11fad0a7ad923da755ee596b428a
Nyan33 <volfnikita1304@gmail.com>
2022-01-07 18:30:49 +0300

    chances init

--- commands/wish.js
+++ commands/wish.js
@@ -2,97 +2,126 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.names = exports.run = void 0;
 function run(e) {
+    var d = Math.random();
     var roll = (Math.floor(Math.random() * 13) + 1);
-    if (roll == 1) {
+    console.log('chance: ' + d, '  roll: ' + roll);
+    if (roll == 1 && d < 0.5) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/1.jpg"
             ]
         });
     }
-    if (roll == 2) {
+    else if (roll == 1 && d > 0.5) {
+        e.message.reply(`ты ничего выбил...`);
+    }
+    else if (roll == 2 && d < 0.6) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/2.jpg"
             ]
         });
     }
-    if (roll == 3) {
+    else if (roll == 3 && d < 0.6) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/3.jpg"
             ]
         });
     }
-    if (roll == 4) {
+    else if (roll == 4 && d < 0.6) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/4.jpg"
             ]
         });
     }
-    if (roll == 5) {
+    else if (roll == 5 && d < 0.6) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/5.jpg"
             ]
         });
     }
-    if (roll == 6) {
+    else if (roll == 2 || roll == 3 || roll == 4 || roll == 5 && d > 0.6) {
+        e.message.reply(`ты ничего выбил...`);
+    }
+    else if (roll == 6 && d < 0.1) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/6.jpg"
             ]
         });
     }
-    if (roll == 7) {
+    else if (roll == 6 && d > 0.1) {
+        e.message.reply(`ты ничего выбил...`);
+    }
+    else if (roll == 7 && d < 0.09) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/7.jpg"
             ]
         });
     }
-    if (roll == 8) {
+    else if (roll == 7 && d > 0.09) {
+        e.message.reply(`ты ничего выбил...`);
+    }
+    else if (roll == 8 && d < 0.9) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/8.jpg"
             ]
         });
     }
-    if (roll == 9) {
+    else if (roll == 9 && d < 0.9) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/9.jpg"
             ]
         });
     }
-    if (roll == 10) {
+    else if (roll == 8 || roll == 9 && d > 0.9) {
+        e.message.reply(`ты ничего выбил...`);
+    }
+    else if (roll == 10 && d < 0.01) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/10.mp4"
             ]
         });
     }
-    if (roll == 11) {
+    else if (roll == 10 && d > 0.1) {
+        e.message.reply(`ты ничего выбил...`);
+    }
+    else if (roll == 11 && d < 0.01) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/11.mp4"
             ]
         });
     }
-    if (roll == 12) {
+    else if (roll == 11 && d > 0.1) {
+        e.message.reply(`ты ничего выбил...`);
+    }
+    else if (roll == 12 && d < 0.1) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/12.png"
             ]
         });
     }
-    if (roll == 13) {
+    else if (roll == 12 && d > 0.1) {
+        e.message.reply(`ты ничего выбил...`);
+    }
+    else if (roll == 13 && d < 0.11) {
         e.message.reply(`ты выбил`, {
             files: [
                 "./pics/13.jpg"
             ]
         });
+    }
+    else if (roll == 13 && d > 0.11) {
+        e.message.reply(`ты ничего выбил...`);
     }
 }
 exports.run = run;
