a27914a1c8976263b1ab01772501dd0f435954ae
Nyan33 <volfnikita1304@gmail.com>
2022-01-07 19:05:28 +0300

    vote cm

---
+++ commands/vote.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.names = exports.run = void 0;
+function run(e) {
+    e.message.reply(`Нажмите на Vote и поставьте вашу оценку боту!\nГолосовать: https://top.gg/bot/928689274356891668`);
+}
+exports.run = run;
+exports.names = ["топ.гг"];
---
+++ commands/vote.ts
@@ -0,0 +1,6 @@
+import {runEvent} from "../index";
+
+export function run(e:runEvent) {
+    e.message.reply(`Нажмите на Vote и поставьте вашу оценку боту!\nГолосовать: https://top.gg/bot/928689274356891668`) }
+
+export const names = ["топ.гг"];
